caa98fb5422427541418e6b510dcfab336b42cc0
Yonathan Koren <yonkornilov@live.com>
2018-07-18 17:50:48 -0400

    successfuly unit test for hostname syscall working in new UTS namespace

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"log"
 	"syscall"
 )
 
-func Host(hostname string) error {
-	fmt.Printf("Setting hostname to: %s\n", hostname)
+func Host(hostname string) (string, error) {
 	err := syscall.Sethostname([]byte(hostname))
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	hostname, err = os.Hostname()
+	if err != nil {
+		return "", err
+	}
+	return hostname, nil
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 2 && args[0] == "host"  {
+		hostname, err := Host(args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(hostname)
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"strings"
+	"os"
+	"os/exec"
+	"testing"
+	"syscall"
+)
+
+func TestHost(t *testing.T) {
+	oldHostname, err := os.Hostname()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var hostname string = "test"
+	cmd := exec.Command("go", "run", "main.go", "host", hostname)
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUSER |
+                        syscall.CLONE_NEWUTS,
+		UidMappings: []syscall.SysProcIDMap{
+                        {
+                                ContainerID: 0,
+                                HostID:      os.Getuid(),
+                                Size:        1,
+                        },
+                },
+                GidMappings: []syscall.SysProcIDMap{
+                        {
+                                ContainerID: 0,
+                                HostID:      os.Getgid(),
+                                Size:        1,
+                        },
+                },
+	}
+	output, err := cmd.Output()
+	if err != nil {
+		t.Fatal(err)
+	}
+	outputString := strings.Trim(string(output), "\n")
+	if  outputString != hostname {
+		t.Fatalf("Expected hostname to be '%s' after Host(%s), got '%s'", hostname, hostname, output)
+	}
+	t.Logf("Hostname was '%s', successfuly changed it to '%s' in new UTS namespace", oldHostname, outputString)
+}
